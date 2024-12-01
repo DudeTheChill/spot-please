45046c7af0365d9aebfda7230cb15514ce455a8f
Mohammad Mehdi Mohammadpour <mm.mohammadpour@gmail.com>
2024-12-01 22:25:38 +0100

    events module added

--- spotplease/settings.py
+++ spotplease/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'events.apps.EventsConfig'
 ]
 
 MIDDLEWARE = [
