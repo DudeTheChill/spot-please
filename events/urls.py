8ca9ce9a2954ccef147ff0d48307bff23c2e5458
Mohammad Mehdi Mohammadpour <mm.mohammadpour@gmail.com>
2025-01-01 22:10:46 +0100

    small additions to events folder

--- events/urls.py
+++ events/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import 
+from .views import event_choices
 
 urlpatterns = [
-    path('', , name="events-home"),
-    path('create_choices/', , name="event-create"),
-    path('create/', , name="event-form"),
-    path('create_with_eventbrite/', , name="event-brite"),
-    path('update/<int:pk>/', , name="event-update"),
-    path('delete/<int:pk>/', , name="event-delete"),
-    path('delete_confirmation/<int:pk>/', , name="event-delete-confirm"),
-    path('<int:pk>/', , name="event-details"),
+    #path('', , name="events-home"),
+    path('create_choices/', event_choices, name="event-create"),
+    #path('create/', , name="event-form"),
+    #path('create_with_eventbrite/', , name="event-brite"),
+    #path('update/<int:pk>/', , name="event-update"),
+    #path('delete/<int:pk>/', , name="event-delete"),
+    #path('delete_confirmation/<int:pk>/', , name="event-delete-confirm"),
+    #path('<int:pk>/', , name="event-details"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def event_choices(request):
+    return render(request, 'events/event_create_choices.html')
